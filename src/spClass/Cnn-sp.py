d42df13d36cf0833f2ff22d7afd19849801b58b0
Passakorn Pattarapakorn <63011240@kmitl.ac.th>
2022-11-14 20:50:49 +0700

    new file:   runs/Nov14_13-20-42_LAPTOP-ILPNQLTT1668406842.7180388/events.out.tfevents.1668406844.LAPTOP-ILPNQLTT.9876.0 	new file:   runs/Nov14_13-29-47_LAPTOP-ILPNQLTT1668407387.7433403/events.out.tfevents.1668407390.LAPTOP-ILPNQLTT.24140.0 	modified:   src/spClass/Cnn-sp.py 	new file:   src/spClass/Modelss.py 	new file:   src/spClass/summary.txt 	new file:   utils.zip

--- src/spClass/Cnn-sp.py
+++ src/spClass/Cnn-sp.py
@@ -1,20 +1,23 @@
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+
+from Modelss import MyModel
 
 #Tensor board dependencies
+
+import time
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter('path_to_logs')
+
+writer = SummaryWriter(comment = str(time.time()))
 
 from utils import AverageMeterSet
 meters = AverageMeterSet()
 meters.reset()
 
-print('Start the code Here')
-print(f'Torch version is {torch.__version__}')
+import matplotlib.pyplot as plt
+import numpy as np
 
 ds_dir = 'C:\\Users\\Ribuzari\\Documents\\KMITL\\git\\src\\materials\\dataset'
 target = 'C:\\Users\\Ribuzari\\Documents\\KMITL\\git\\src\\materials\\testDataset'
@@ -22,7 +25,7 @@ img_height,img_width=224,224
 
 ds = torchvision.datasets.ImageFolder(ds_dir)
 
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 
 ## transformations
 transform = transforms.Compose([
@@ -41,68 +44,18 @@ testset = torchvision.datasets.ImageFolder(root=target,
 testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
                                          shuffle=False, num_workers=0)
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-## functions to show an image
-def imshow(img):
-    #img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-## get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-## show images
-imshow(torchvision.utils.make_grid(images))
-
 for images, labels in trainloader:
     print("Image batch dimensions:", images.shape)
     print("Image label dimensions:", labels.shape)
     break
 
-class MyModel(nn.Module):
-    def __init__(self, image_size):
-        super(MyModel, self).__init__()
-        self.image_size = image_size
-        self.img_size = image_size - 2
-        # declare layer
-        # 28x28x1 => 26x26x32
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3) # can set acti arg
-        self.d1 = nn.Linear(self.img_size * self.img_size * 32, 128)
-        self.d2 = nn.Linear(128, 15)# ?? -> output(classes = 15)
-
-    def forward(self, x):
-        # 32x1x28x28 => 32x32x26x26
-        x = self.conv1(x)
-        x = F.relu(x)
-
-        # flatten => 32 x (32*26*26)
-        x = x.flatten(start_dim = 1)
-
-        # 32 x (32*26*26) => 32x128
-        x = self.d1(x)
-        x = F.relu(x)
-
-        # logits => 32x10
-        logits = self.d2(x)
-        out = F.softmax(logits, dim=1)
-        return out
-
-model = MyModel(img_height)
-for images, labels in trainloader:
-    print("batch size:", images.shape)
-    out = model(images)
-    print(out.shape)
-    break
 
 
 learning_rate = 0.001
 num_epochs = 2
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = MyModel(img_height)
+model = MyModel()
 model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -138,7 +91,7 @@ for epoch in range(num_epochs):
         train_running_loss += loss.detach().item()
         train_acc += get_accuracy(logits, labels, BATCH_SIZE)
 
-        writer.add_scalar('New loss',loss.detach().item(),count+i)
+        writer.add_scalar('another loss',loss.detach().item(),count+i)
         meters.update('Batch loss', loss.detach().item())
         print('batch: %d | Loss: %.4f | count: %d' \
           %(i, loss.detach().item(),count+i+1))
@@ -155,10 +108,11 @@ for i, (images, labels) in enumerate(testloader, 0):
     labels = labels.to(device)
     outputs = model(images)
     test_acc += get_accuracy(outputs, labels, BATCH_SIZE)
+    count = i+1
         
-print('Test Accuracy: %.2f'%( test_acc/i))
+print('Test Accuracy: %.2f'%( test_acc/count))
 
-print('result = {}'.format(meters['Batch loss']))
-
+# print('result = {}'.format(meters['Batch loss']))
+writer.close()
 print(meters.counts())
 print(meters.metrics)
---
+++ src/spClass/Modelss.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+
+        # declare layer
+        # 28x28x3 => 26x26x32
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3) # can set acti arg
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3) # can set acti arg
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3) # can set acti arg
+        self.maxpool3 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        
+        self.d1 = nn.Linear(86528, 128)
+        self.d2 = nn.Linear(128, 15)# ?? -> output(classes = 15)
+
+    def forward(self, x):
+        # 32x1x28x28 => 32x32x26x26
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.maxpool3(x)
+
+        # flatten => 32 x (32*26*26)
+        x = x.flatten(1)
+
+        # 32 x (32*26*26) => 32x128
+        x = self.d1(x)
+        x = F.relu(x)
+
+        # logits => 32x10
+        logits = self.d2(x)
+        out = F.softmax(logits, dim=1)
+        return logits
